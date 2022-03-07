4d916f399c6d8b4ca189000b86d232f95897a94a
seorlando33 <seorlando33@gmail.com>
2022-03-07 17:24:05 -0300

    Server Structure Finished

--- api/server.js
+++ api/server.js
@@ -1,1 +1,22 @@
 const express = require('express')
+const cors = require("cors")
+
+const routes = require("./routes")
+const { PORT } = require("./config/config")
+
+const app = express()
+
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.use("/", routes)
+
+app.listen(PORT, () => {
+    console.log(`Server's listening on Port: ${PORT}`)
+})
