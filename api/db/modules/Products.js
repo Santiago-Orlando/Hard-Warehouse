d4ed40a61b9924d3a603fa7e583f36e5ee3b89d3
seorlando33 <seorlando33@gmail.com>
2022-03-08 01:30:30 -0300

    Database Connection & Product Model

---
+++ api/db/modules/Products.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose")
+
+
+const ProductSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+    details: {
+        type: String,
+        default: ""
+    },
+    tags: { type: [String], index: true },
+
+})
+
+
+const ProductModel = model("Products", ProductSchema)
+
+module.exports = ProductModel
