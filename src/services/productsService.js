d5d15d0580af1fa1c317df9221409cf8c1d47a06
Agustinsant <agustinmsant@gmail.com>
2022-03-15 12:10:03 -0300

    search module

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -19,3 +19,21 @@ export const saleProductsService = async () => {
   const saleProducts = await axios.get(`https://fakestoreapi.com/products/category/men's clothing`)
   return saleProducts.data
 }
+
+export const searchProductsByTag = async (tag) => {
+  const searchedProducts =  await axios.get(`http://localhost:3001/search/?q=tags=${tag}`)
+  return searchedProducts.data
+}
+
+export const searchProductsByTitle = async (tag) => {
+  const searchedProducts =  await axios({
+    method: "GET",
+    data: { tags: tag },
+    withCredentials: true,
+    url: "http://localhost:3001/products/showProducts",
+  })
+  console.log ('RESULTADO BACK', searchedProducts.data)
+  return searchedProducts.data
+}
+
+
